acd86e19ccdb84e0ed817bbdcf4deebd9ec41754
Alexander Shuglaevksii <a.shugalevskii@gmail.com>
2024-02-16 15:15:15 +0700

    added  Logger  observer behavior to sync ui with task manager

--- Logger/Logger.cs
+++ Logger/Logger.cs
@@ -1,27 +1,65 @@
-﻿namespace Logger
+﻿namespace LoggerSpace
 {
+    public interface ILoggerObserver
+    {
+        void OnLogUpdated();
+    }
+
     public class Logger
     {
         private Logger() { }
-        private static Logger instance;
+        private static Logger _instance;
 
         public static Logger Instance()
         {
-            if (instance == null)
-            {
-                instance = new Logger();
-            }
-            return instance;
+            return _instance ?? (_instance = new Logger());
         }
         public List<string> LogHistory { get; } = new();
+        private readonly List<ILoggerObserver> _observers = new();
 
         public void Clear()
         {
             LogHistory.Clear();
         }
+
+        public void AddObserver(ILoggerObserver observer)
+        {
+            _observers.Add(observer);
+        }
+        public void RemoveObserver(ILoggerObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public static string AddCurrentTime(string message)
+        {
+            var time = DateTime.Now;
+            var timeStampedMessage = $"{time.Hour:D2}:{time.Minute:D2}:{time.Second:D2} {message}\n";
+            return timeStampedMessage;
+        }
+        
+        public void TimedMessage(string message)
+        {
+            lock (_instance) //bad too
+            {
+                Message(AddCurrentTime(message));
+            }
+        }
+
         public void Message(string message)
         {
-            LogHistory.Add(message);
+            lock (_instance)
+            {
+                LogHistory.Add(message);
+                NotifyObservers();
+            }
+        }
+        private void NotifyObservers()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.OnLogUpdated();
+            }
         }
     }
 }
